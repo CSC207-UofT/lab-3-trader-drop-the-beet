cbaded578cf24ff36b7af915e071e140a4323e62
yy-7105 <leoyang105@gmail.com>
2021-09-27 17:19:49 -0400

    Yeye

---
+++ src/test/java/DragonTest.java
@@ -0,0 +1,40 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class DragonTest {
+    Dragon d;
+
+    @Before
+    public void setUp() throws Exception {
+        d = new Dragon();
+    }
+
+    @Test(timeout = 50)
+    public void TestSound() {
+        assertEquals("Rua!", d.sound());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetMaxSpeed() {
+        assertEquals(100, d.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestUpgradeSpeed() {
+        d.upgradeSpeed();
+        assertEquals(105, d.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestDowngradeSpeed() {
+        d.downgradeSpeed();
+        assertEquals(95, d.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetPrice() {
+        assertEquals(114514, d.getPrice());
+    }
+
+}
